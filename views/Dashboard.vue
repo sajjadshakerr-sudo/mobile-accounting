9a508c2849524cd3a838a0111ce3d45a5938aa0f
sajjadshakerr-sudo <sajjadshakerr@gmail.com>
2026-08-21 17:01:27 +0330

    Update Dashboard.vue

--- views/Dashboard.vue
+++ views/Dashboard.vue
@@ -845,3 +845,4 @@ const clearAllData = async () => {
 .btn-secondary { background-color: var(--border); color: var(--text-dark); }
 @keyframes modalScaleIn { 0% { opacity: 0; transform: scale(0.95) translateY(10px); } 100% { opacity: 1; transform: scale(1) translateY(0); } }
 </style>
+
